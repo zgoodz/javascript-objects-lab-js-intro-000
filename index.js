c9b409afbcb73a97f42744ed8aa768e23ce310d3
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-05-02 00:44:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
- return Object.assign({}, object, )
+ return Object.assign({}, object, {key:value})
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
- 
+ return object['key']=value
 }
 
 function deleteFromObjectByKey(object, key) {
