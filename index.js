de0b51d6d87f693ac46b8c3b7cc1a1b3fb0d4c6f
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-05-02 01:47:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function deleteFromObjectByKey(object, key) {
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
-  object = delete object.key
+  delete object[key]
+  return object
   
 }
