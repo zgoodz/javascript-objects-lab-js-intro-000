664d93048b7caa8ef2b5e0f738f3a5a1e019b70f
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-05-02 01:19:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 function updateObjectWithKeyAndValue(object, key, value) {
- Object.assign(object, {['key']:value})
+ Object.assign({}, object, {['key']:value})
  return object
 }
 
