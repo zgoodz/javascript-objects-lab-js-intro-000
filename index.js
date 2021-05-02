e6de816df19df796d94dd18b056d57e2ff297921
Zachary Goodman <zacharyrgoodman@gmail.com>
2021-05-02 00:44:28 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+var recipes = {}
+
+function updateObjectWithKeyAndValue(object, key, value) {
+ return Object.assign({}, object, )
+}
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+ 
+}
+
+function deleteFromObjectByKey(object, key) {
+  return delete object.key;
+}
+
+function destructiveltyDeleteFromObjectByKey(object,key) {
+  
+}
